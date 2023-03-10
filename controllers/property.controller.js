d1332e1c575e861192aafbe92f70fc66b891c8f9
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-10 13:38:59 +0200

    updateProperty function done..

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -48,7 +48,7 @@ const getAllProperties = async (req, res) => {
 };
 
 const getPropertyDetail = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params;  
   const propertyExists = await Property.findOne({ _id: id }).populate(
     "creator"
   );
@@ -92,7 +92,28 @@ const createProperty = async (req, res) => {
   }
 };
 
-const updateProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {title, description, propertyType, location, price, photo} = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    await Property.findByIdAndUpdate({_id: id}, {
+      title,
+      description,
+      propertyType,
+      location,
+      photo: photoUrl.url || photo
+    })
+
+    res.status(200).json({message: "Property updated successfully"})
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+
+
+
+};
 
 
 const deleteProperty = async (req, res) => {
